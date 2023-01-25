5388614a1b0f3926080ebb6e10df3348217021c4
Denis Bashkatov <d.bashkatov@tinkoff.ru>
2023-01-25 20:58:26 +0300

    feat: added json format

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,33 +1,52 @@
 import path from 'path';
 import flat from '../src/gendiff.js';
-import { stylishResult, plainResult } from '../__fixtures__/results.js';
+import {
+  stylishResult,
+  plainResult,
+  jsonResult,
+} from '../__fixtures__/results.js';
 
 const getFixturePath = (filename) => path.join('__fixtures__', filename);
+describe('gendiff', () => {
+  test('stylish json diff', () => {
+    const filepath1 = getFixturePath('file1.json');
+    const filepath2 = getFixturePath('file2.json');
 
-test('stylish json diff', () => {
-  const filepath1 = getFixturePath('file1.json');
-  const filepath2 = getFixturePath('file2.json');
+    expect(flat(filepath1, filepath2, 'stylish')).toBe(stylishResult);
+  });
 
-  expect(flat(filepath1, filepath2, 'stylish')).toBe(stylishResult);
-});
+  test('stylish format yaml file diff', () => {
+    const filepath1 = getFixturePath('file1.yml');
+    const filepath2 = getFixturePath('file2.yml');
 
-test('stylish yaml diff', () => {
-  const filepath1 = getFixturePath('file1.yml');
-  const filepath2 = getFixturePath('file2.yml');
+    expect(flat(filepath1, filepath2, 'stylish')).toBe(stylishResult);
+  });
 
-  expect(flat(filepath1, filepath2, 'stylish')).toBe(stylishResult);
-});
+  test('plain format json file diff', () => {
+    const filepath1 = getFixturePath('file1.json');
+    const filepath2 = getFixturePath('file2.json');
 
-test('plain json diff', () => {
-  const filepath1 = getFixturePath('file1.json');
-  const filepath2 = getFixturePath('file2.json');
+    expect(flat(filepath1, filepath2, 'plain')).toBe(plainResult);
+  });
 
-  expect(flat(filepath1, filepath2, 'plain')).toBe(plainResult);
-});
+  test('plain format yaml file diff', () => {
+    const filepath1 = getFixturePath('file1.yml');
+    const filepath2 = getFixturePath('file2.yml');
 
-test('plain yaml diff', () => {
-  const filepath1 = getFixturePath('file1.yml');
-  const filepath2 = getFixturePath('file2.yml');
+    expect(flat(filepath1, filepath2, 'plain')).toBe(plainResult);
+  });
 
-  expect(flat(filepath1, filepath2, 'plain')).toBe(plainResult);
+  test('json format json file diff', () => {
+    const filepath1 = getFixturePath('file1.json');
+    const filepath2 = getFixturePath('file2.json');
+
+    expect(flat(filepath1, filepath2, 'plain')).toBe(plainResult);
+  });
+
+  test('json format yaml file diff', () => {
+    const filepath1 = getFixturePath('file1.yml');
+    const filepath2 = getFixturePath('file2.yml');
+
+    expect(flat(filepath1, filepath2, 'json')).toBe(jsonResult);
+  });
 });
